6fd767f54908a224414820ec910d13c176f2fd09
hasitha prasad <prasadhasitha80@gmail.com>
2025-08-09 17:12:37 +0530

    Add files via upload

---
+++ pair.js
@@ -0,0 +1,131 @@
+const express = require("express");
+const fs = require("fs");
+const { exec } = require("child_process");
+let router = express.Router();
+const pino = require("pino");
+const {
+  default: makeWASocket,
+  useMultiFileAuthState,
+  delay,
+  makeCacheableSignalKeyStore,
+  Browsers,
+  jidNormalizedUser,
+} = require("@whiskeysockets/baileys");
+const { upload } = require("./mega");
+
+function removeFile(FilePath) {
+  if (!fs.existsSync(FilePath)) return false;
+  fs.rmSync(FilePath, { recursive: true, force: true });
+}
+
+router.get("/", async (req, res) => {
+  let num = req.query.number;
+  async function HasiPair() {
+    const { state, saveCreds } = await useMultiFileAuthState(`./session`);
+    try {
+      let HasiPairWeb = makeWASocket({
+        auth: {
+          creds: state.creds,
+          keys: makeCacheableSignalKeyStore(
+            state.keys,
+            pino({ level: "fatal" }).child({ level: "fatal" })
+          ),
+        },
+        printQRInTerminal: false,
+        logger: pino({ level: "fatal" }).child({ level: "fatal" }),
+        browser: Browsers.macOS("Safari"),
+      });
+
+      if (!HasiPairWeb.authState.creds.registered) {
+        await delay(1500);
+        num = num.replace(/[^0-9]/g, "");
+        const code = await HasiPairWeb.requestPairingCode(num);
+        if (!res.headersSent) {
+          await res.send({ code });
+        }
+      }
+
+      HasiPairWeb.ev.on("creds.update", saveCreds);
+      HasiPairWeb.ev.on("connection.update", async (s) => {
+        const { connection, lastDisconnect } = s;
+        if (connection === "open") {
+          try {
+            await delay(10000);
+            const sessionPrabath = fs.readFileSync("./session/creds.json");
+
+            const auth_path = "./session/";
+            const user_jid = jidNormalizedUser(HasiPairWeb.user.id);
+
+            function randomMegaId(length = 6, numberLength = 4) {
+              const characters =
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+              let result = "";
+              for (let i = 0; i < length; i++) {
+                result += characters.charAt(
+                  Math.floor(Math.random() * characters.length)
+                );
+              }
+              const number = Math.floor(
+                Math.random() * Math.pow(10, numberLength)
+              );
+              return `${result}${number}`;
+            }
+
+            const mega_url = await upload(
+              fs.createReadStream(auth_path + "creds.json"),
+              `${randomMegaId()}.json`
+            );
+
+            const string_session = mega_url.replace(
+              "https://mega.nz/file/",
+              ""
+            );
+
+            const sid = `*D&D Commiunication BOT*\n\n👉 ${string_session} 👈\n\nWel Come`;
+            const mg = `🛑 *Do not share this code to anyone* 🛑`;
+            const dt = await HasiPairWeb.sendMessage(user_jid, {
+              image: {
+                url: "https://raw.githubusercontent.com/hasi132-jpg/logo/refs/heads/main/d%20d%20commiunication1.jpg",
+              },
+              caption: sid,
+            });
+            const msg = await HasiPairWeb.sendMessage(user_jid, {
+              text: string_session,
+            });
+            const msg1 = await HasiPairWeb.sendMessage(user_jid, { text: mg });
+          } catch (e) {
+            exec("pm2 restart Hasi");
+          }
+
+          await delay(100);
+          return await removeFile("./session");
+          process.exit(0);
+        } else if (
+          connection === "close" &&
+          lastDisconnect &&
+          lastDisconnect.error &&
+          lastDisconnect.error.output.statusCode !== 401
+        ) {
+          await delay(10000);
+          HasiPair();
+        }
+      });
+    } catch (err) {
+      exec("pm2 restart Hasi-md");
+      console.log("service restarted");
+      HasiPair();
+      await removeFile("./session");
+      if (!res.headersSent) {
+        await res.send({ code: "Service Unavailable" });
+      }
+    }
+  }
+  return await HasiPair();
+});
+
+process.on("uncaughtException", function (err) {
+  console.log("Caught exception: " + err);
+  exec("pm2 restart Robin");
+});
+
+module.exports = router;
